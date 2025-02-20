a7f0ae2171f60355a2a6b32c4d478161afc7d564
Alexey <practicemakesperfect73@gmail.com>
2025-02-20 23:33:56 +0900

    Загрузил файлы с кодом

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,37 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
-from django.shortcuts import render
+from datacenter.models import Visit, Passcard
+from django.utils.timezone import localtime, now
+from django.shortcuts import render, get_object_or_404
+import datetime
+
+
+def format_duration(duration):
+    seconds = duration.total_seconds()
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    return f'{int(hours)}ч {int(minutes)}м'
+
+
+def is_visit_long(visit, minutes=60):
+    if visit.leaved_at is not None:
+        end_time = visit.leaved_at
+    else:
+        end_time = now()
+    return (end_time - visit.entered_at) >= datetime.timedelta(minutes=minutes)
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+    passcard = get_object_or_404(Passcard, passcode=passcode)  
+    visits = Visit.objects.filter(passcard=passcard).order_by('-entered_at')
 
     this_passcard_visits = [
         {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
+            'entered_at': localtime(visit.entered_at),
+            'duration': format_duration((visit.leaved_at or now()) - visit.entered_at),
+            'is_strange': is_visit_long(visit),
+        }
+        for visit in visits
     ]
+
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,32 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
+from datacenter.models import Visit, Passcard
 from django.shortcuts import render
+from django.utils.timezone import localtime, now
+
+
+def get_duration(visit):
+    delta = now() - visit.entered_at
+    return delta
+
+
+def format_duration(duration):
+    seconds = duration.total_seconds()
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    return f'{int(hours)}ч {int(minutes)}м'
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    current_visitors = Visit.objects.filter(leaved_at__isnull=True, passcard__is_active=True)
+    non_closed_visits = []
+    for visitor in current_visitors:
+        duration = get_duration(visitor)
+        non_closed_visits.append({
+            'who_entered': visitor.passcard,
+            'entered_at': localtime(visitor.entered_at),
+            'duration': format_duration(duration),
+        })
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
